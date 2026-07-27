efca648ecd17ba0281e63436a0369101a64c18cc
NOTZ112 <liza3x3i@gmail.com>
2026-07-27 23:33:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
 
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+
+MONGO_URI = os.environ["MONGO_URI"]
+
+ADMIN_ID = int(os.environ["ADMIN_ID"])
